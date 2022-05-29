f178ff50c1ea29902820aab19b8ba5eb069d9e00
Aleksandr Petrik <alexangel102@gmail.com>
2022-05-29 19:04:23 +0300

    Preparing for Packagist

--- src/errorHandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-function exceptions_error_handler($severity, $message, $filename, $lineno) {
-    throw new ErrorException($message, 0, $severity, $filename, $lineno);
-}
-
-set_error_handler('exceptions_error_handler');
-
-libxml_use_internal_errors(true);
